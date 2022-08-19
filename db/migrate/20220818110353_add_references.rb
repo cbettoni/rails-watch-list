8982b97688f16bca5be65358178e9933468e2156
christelle bettoni <christelle.bettoni@gmail.com>
2022-08-19 15:25:52 +0200

    des trucs

--- db/migrate/20220818110353_add_references.rb
+++
@@ -1,6 +0,0 @@
-class AddReferences < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :bookmarks, :movie, foreign_key: true
-    add_reference :bookmarks, :list, foreign_key: true
-  end
-end
