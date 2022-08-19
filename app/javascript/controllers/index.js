e6a57ddc64829f5c7531377c66074567a920e6be
christelle bettoni <christelle.bettoni@gmail.com>
2022-08-19 15:43:17 +0200

    heroku

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,18 +3,3 @@
 // ./bin/rails generate stimulus controllerName
 
 import { application } from "./application"
-
-import JqueryBarRatingController from "./jquery_bar_rating_controller"
-application.register("jquery-bar-rating", JqueryBarRatingController)
-
-import Select2Controller from "./select2_controller"
-application.register("select2", Select2Controller)
-
-import StarRatingController from "./star_rating_controller"
-application.register("star-rating", StarRatingController)
-
-import TomSelectController from "./tom_select_controller"
-application.register("tom-select", TomSelectController)
-
-import TypedJsController from "./typed_js_controller"
-application.register("typed-js", TypedJsController)
